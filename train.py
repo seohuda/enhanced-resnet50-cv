d9e9826082df7a493f05981d212ec3383fb7940b
seohuda <seohu6640@gmail.com>
2026-06-14 09:45:00 +0900

    refactor: extract utility functions to utils.py

--- train.py
+++ train.py
@@ -7,51 +7,7 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 from model import build_se_resnet50
-
-
-def cutmix_data(x, y, alpha=1.0):
-    lam = np.random.beta(alpha, alpha)
-    batch_size = x.size(0)
-    index = torch.randperm(batch_size).to(x.device)
-
-    y_a, y_b = y, y[index]
-
-    bbx1, bby1, bbx2, bby2 = rand_bbox(x.size(), lam)
-    x[:, :, bbx1:bbx2, bby1:bby2] = x[index, :, bbx1:bbx2, bby1:bby2]
-
-    lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (x.size(-1) * x.size(-2)))
-
-    return x, y_a, y_b, lam
-
-
-def rand_bbox(size, lam):
-    W = size[2]
-    H = size[3]
-    cut_rat = np.sqrt(1.0 - lam)
-    cut_w = int(W * cut_rat)
-    cut_h = int(H * cut_rat)
-
-    cx = np.random.randint(W)
-    cy = np.random.randint(H)
-
-    bbx1 = np.clip(cx - cut_w // 2, 0, W)
-    bby1 = np.clip(cy - cut_h // 2, 0, H)
-    bbx2 = np.clip(cx + cut_w // 2, 0, W)
-    bby2 = np.clip(cy + cut_h // 2, 0, H)
-
-    return bbx1, bby1, bbx2, bby2
-
-
-class SoftLabelCrossEntropyLoss(nn.Module):
-    def __init__(self):
-        super(SoftLabelCrossEntropyLoss, self).__init__()
-
-    def forward(self, outputs, targets_a, targets_b, lam):
-        log_probs = nn.functional.log_softmax(outputs, dim=1)
-        loss_a = nn.functional.nll_loss(log_probs, targets_a)
-        loss_b = nn.functional.nll_loss(log_probs, targets_b)
-        loss = lam * loss_a + (1 - lam) * loss_b
-        return loss
+from utils import cutmix_data, SoftLabelCrossEntropyLoss
 
 
 def get_dataloaders(batch_size=128):
